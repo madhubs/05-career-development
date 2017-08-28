ac281b436b2ecfea930a347b48e763988969db80
Madeline Stevens <stevensm1987@gmail.com>
2017-08-28 08:51:35 -0700

    trying to add

---
+++ lab-maddy/listarray.js
@@ -0,0 +1,13 @@
+'use strict';
+
+// Create a List constructor that is a replacement for Arrays
+// Add push, pop, map, filter, reduce, and slice methods to the constructor
+
+function Animals (name, species) {
+  this.name = name;
+  this.species = species;
+}
+
+listArray.prototype.filter = function () {
+
+};
